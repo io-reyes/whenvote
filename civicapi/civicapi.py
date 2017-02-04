2cc894b0dfd3380f1b98b140d651cae0807d0672
ioreyes <ioreyes@icsi.berkeley.edu>
2017-02-03 17:45:31 -0800

    Generate the election data file as a static asset

--- civicapi/civicapi.py
+++ civicapi/civicapi.py
@@ -36,35 +36,42 @@ class CivicAPI:
 
         return data
         
+    def _level_filter(self, level_name):
+        """Accept state and federal elections"""
+        lower_level = level_name.lower()
+        return lower_level.startswith('state') or lower_level.startswith('federal')
 
     def _request_level_mapping(self):
-        """Get the mapping of state and federal election levels to level IDs"""
+        """Get the mapping of election levels to level IDs"""
         if(self._level_mapping != None):
             return self._level_mapping
         else:
             levels = self._request_all('election_levels')
             if(len(levels) > 0):
-                self._level_mapping =  {x['name']:x['id'] for x in levels if x['name'].startswith('State') or x['name'].startswith('Federal')}
+                self._level_mapping =  {x['name']:x['id'] for x in levels if self._level_filter(x['name'])}
                 return self._level_mapping
             else:
                 return {}
+
+    def _has_date_passed(self, api_date):
+        """Returns true if the date (in the API's YYYY-MM-DD format) is in the past"""
+        now = datetime.now()
+        now_date_string = '%04d-%02d-%02d' % (now.year, now.month, now.day)
+
+        return api_date < now_date_string
 
     def _request_elections(self):
         """Get all elections in the levels specified by the mapping"""
         level_mapping = self._request_level_mapping()
 
-        # Use this to filter out all resulting elections that have already happened
-        now = datetime.now()
-        now_date_string = '%04d-%02d-%02d' % (now.year, now.month, now.day)
-
         elections = []
         for name,levid in level_mapping.items():
-            elections_in_level = [x for x in self._request_all('elections', params={'election_level_id':levid}) if x['election_date'] >= now_date_string]
+            elections_in_level = [x for x in self._request_all('elections', params={'election_level_id':levid}) if not self._has_date_passed(x['election_date'])]
             elections.extend(elections_in_level)
 
         return elections
 
-    def get_upcoming_elections(self):
+    def _request_upcoming_elections(self):
         """For each state, return only the most immediate upcoming elections"""
         elections = self._request_elections()
 
@@ -77,3 +84,63 @@ class CivicAPI:
 
         return upcoming_elections
 
+    def _date_split(self, api_date):
+        """Splits the API's YYYY-MM-DD date format into a dict with integer year, month, and day entries"""
+        components = api_date.split('-')
+
+        if(len(components) == 3):
+            return {'year':int(components[0]), 'month':int(components[1]), 'day':int(components[2])}
+        else:
+            return {'year':-1, 'month':-1, 'day':-1}
+
+
+    def _get_deadlines(self, state_elections):
+        all_deadlines = []
+        for election in state_elections:
+            all_deadlines.extend(election['dates'])
+
+        upcoming_deadlines = [x for x in all_deadlines if x['date'] is not None and not self._has_date_passed(x['date'])]
+        registration_deadlines = [x['date'] for x in upcoming_deadlines if x['kind'] == 'DRD']
+        absentee_deadlines = [x['date'] for x in upcoming_deadlines if x['kind'] == 'DBRD']
+
+        registration_deadline = self._date_split(min(registration_deadlines)) if len(registration_deadlines) > 0 else self._date_split('')
+        absentee_deadline = self._date_split(min(absentee_deadlines)) if len(absentee_deadlines) > 0 else self._date_split('')
+
+        return (registration_deadline, absentee_deadline)
+
+
+    def get_upcoming_elections(self):
+        """For each state with an upcoming election, return only the data
+        that will be shown on the site:
+        > State
+        > Election date
+        > Election name(s)
+        > First upcoming domestic registration deadline
+        > First upcoming domestic absentee request deadline, if any
+        """
+        raw_upcoming_elections = self._request_upcoming_elections()
+        state_abbrevs = set([x['state']['short_name'] for x in raw_upcoming_elections])
+        
+        website_elections = {}
+        for state in state_abbrevs:
+            state_elections = [x for x in raw_upcoming_elections if x['state']['short_name'] == state]
+
+            election_date = self._date_split(state_elections[0]['election_date'])
+            election_names = [x['title'] for x in state_elections]
+            register_by, absentee_by = self._get_deadlines(state_elections)
+
+            state_data = {}
+            state_data['election_year'] = election_date['year']
+            state_data['election_month'] = election_date['month']
+            state_data['election_day'] = election_date['day']
+            state_data['election_names'] = election_names
+            state_data['register_year'] = register_by['year']
+            state_data['register_month'] = register_by['month']
+            state_data['register_day'] = register_by['day']
+            state_data['absentee_year'] = absentee_by['year']
+            state_data['absentee_month'] = absentee_by['month']
+            state_data['absentee_day'] = absentee_by['day']
+
+            website_elections[state] = state_data
+
+        return website_elections
--- civicapi/fetch.py
+++ civicapi/fetch.py
@@ -1,22 +1,40 @@
 from civicapi import CivicAPI
+from jsoncompare import jsoncompare
+
 import argparse
+import json
+import os
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('secret', help='text file containing only the secret Civic API token')
+    parser.add_argument('output', help='path and filename of the output JSON file, will overwrite existing file if values are different')
 
     return parser.parse_args()
 
 if __name__ == '__main__':
+    # Get arguments
     args = parse_args()
     secret_file = args.secret
+    out_file = os.path.abspath(args.output)
 
+    # Read in any existing data
+    current_data = None
+    if os.path.isfile(out_file):
+        with open(out_file, 'r') as fp:
+            current_data = json.load(fp)
+
+    # Read the API secret
     secret = None
     with open(secret_file, 'r') as f:
         secret = f.readline().strip()
 
+    # Get election data
     api = CivicAPI(secret)
     data = api.get_upcoming_elections()
 
-    for election in data:
-        print('%s : %s' % (election['title'], election['election_date']))
+    # Write election data if it's changed
+    if(current_data != data):
+        with open(out_file, 'w') as fp:
+            json.dump(data, fp, indent=4)
+            print('Civic Data updated')
