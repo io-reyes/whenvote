15313ba7d0e08ce95841d6f51d844ac054af5e38
ioreyes <ioreyes@icsi.berkeley.edu>
2017-02-04 10:33:43 -0800

    Fixed calendar generation day offset

--- civicapi/icswrite.py
+++ civicapi/icswrite.py
@@ -3,9 +3,9 @@ from ics import Calendar, Event
 class ICSWrite:
     def __init__(self, state_data):
         self.election_title = ', '.join(state_data['election_names'])
-        self.election_date = '%04d%02d%02d 00:00:00' % (state_data['election_year'], state_data['election_month'], state_data['election_day']) if min(state_data['election_year'], state_data['election_month'], state_data['election_day']) > 0 else None
-        self.register_date = '%04d%02d%02d 00:00:00' % (state_data['register_year'], state_data['register_month'], state_data['register_day']) if min(state_data['register_year'], state_data['register_month'], state_data['register_day']) > 0 else None
-        self.absentee_date = '%04d%02d%02d 00:00:00' % (state_data['absentee_year'], state_data['absentee_month'], state_data['absentee_day']) if min(state_data['absentee_year'], state_data['absentee_month'], state_data['absentee_day']) > 0 else None
+        self.election_date = '%04d%02d%02d 23:59:59' % (state_data['election_year'], state_data['election_month'], state_data['election_day']) if min(state_data['election_year'], state_data['election_month'], state_data['election_day']) > 0 else None
+        self.register_date = '%04d%02d%02d 23:59:59' % (state_data['register_year'], state_data['register_month'], state_data['register_day']) if min(state_data['register_year'], state_data['register_month'], state_data['register_day']) > 0 else None
+        self.absentee_date = '%04d%02d%02d 23:59:59' % (state_data['absentee_year'], state_data['absentee_month'], state_data['absentee_day']) if min(state_data['absentee_year'], state_data['absentee_month'], state_data['absentee_day']) > 0 else None
         self.calendar = Calendar()
 
     def _add_event(self, prefix, date):
